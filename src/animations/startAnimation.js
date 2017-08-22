c6dd2874223b4d5985358c5894836f56c0d80649
Roman Terentev <roman_dv_93@mail.ru>
2017-08-22 19:57:00 +0300

    fix classes in start animation fn

--- src/animations/startAnimation.js
+++ src/animations/startAnimation.js
@@ -7,15 +7,17 @@ const startAnimation = () => {
   const head = document.querySelector('.page-header'),
     desc = document.querySelector('.desc'),
     scene = document.querySelector('.scene'),
-    form = document.querySelector('.data__form');
+    controls = document.querySelector('.controls');
 
-  /* timeline for animation */
-  const tl = new TimelineLite();
+  if (head && desc && scene && controls) {
+    /* timeline for animation */
+    const tl = new TimelineLite();
 
-  tl
-    .from(head, 0.5, { y: '-220%' })
-    .from(desc, 0.4, { scale: 0 }, '-=0.2')
-    .from(scene, 0.4, { scale: 0 })
-    .from(form, 0.5, { autoAlpha: 0 });
+    tl
+      .from(head, 0.5, { y: '-220%' }, 0)
+      .from(desc, 0.4, { scale: 0 }, '-=0.2')
+      .from(scene, 0.4, { scale: 0 })
+      .from(controls, 0.5, { autoAlpha: 0 });
+  }
 }
 export default startAnimation;
